fb6a2027fd8aabd495832c744cd048146c33f61f
scho-zenithn <scho@zenithn.com>
2023-10-24 13:56:34 +0900

    deploy to IIS

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,8 +1,9 @@
 from .base import *
 import environ
-import pymssql
 
 ALLOWED_HOSTS = ['zth.kr']
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+CSRF_TRUSTED_ORIGINS = ['https://zth.kr']
